43362168a2ed77a8c1b3b457f165d3a59dc00adf
MartinOM321753 <20223tn139@utez.edu.mx>
2025-05-27 15:50:45 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvomm9b/Cvomm9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvomm9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvomm9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvomm9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvomm9b/Cvomm9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvomm9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvomm9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
